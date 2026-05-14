5228fba9be06ab60a77b25a68e5e76bb3f0d690c
vincenter <vincenterr@yahoo.com.tw>
2026-05-14 20:57:09 +0800

    feat: setup kernel stack to enable function calls

--- os/src/main.rs
+++ os/src/main.rs
@@ -6,3 +6,18 @@ use core::arch::global_asm;
 mod lang_items;
 
 global_asm!(include_str!("entry.asm"));
+
+fn clear_bss() {
+    unsafe extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as *const () as usize..ebss as *const () as usize)
+        .for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
+
+#[unsafe(no_mangle)]
+pub fn rust_main() -> ! {
+    clear_bss();
+    loop {}
+}
