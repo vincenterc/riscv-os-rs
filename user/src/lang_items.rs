0bb3cc17f5b4fa1bca24907ad7d7bd06183c8b1f
vincenter <vincenterr@yahoo.com.tw>
2026-05-23 15:53:44 +0800

    feat: implement apps

---
+++ user/src/lang_items.rs
@@ -0,0 +1,16 @@
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message()
+        );
+    } else {
+        println!("Panicked: {}", info.message());
+    }
+    loop {}
+}
