d86835d72a3177601ab7910ca318bd796dc422f2
vincenter <vincenterr@yahoo.com.tw>
2026-06-21 08:52:36 +0800

    feat: add `initproc` and `user_shell` apps and remove old apps

--- user/src/console.rs
+++ user/src/console.rs
@@ -1,7 +1,8 @@
 use core::fmt::{self, Write};
 
-use super::write;
+use crate::{read, write};
 
+const STDIN: usize = 0;
 const STDOUT: usize = 1;
 
 struct Stdout;
@@ -29,4 +30,10 @@ macro_rules! println {
     ($fmt: literal $(, $($arg: tt)+)?) => {
         $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
     }
+}
+
+pub fn getchar() -> u8 {
+    let mut c = [0u8; 1];
+    read(STDIN, &mut c);
+    c[0]
 }
