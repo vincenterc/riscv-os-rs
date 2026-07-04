32d3e0e0dee8fb64292c074005ed8048c0120a6b
vincenter <vincenterr@yahoo.com.tw>
2026-07-04 10:38:23 +0800

    feat: implement BlockDevice trait on BlockFIle

--- easy-fs-fuse/src/main.rs
+++ easy-fs-fuse/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{Read, Seek, SeekFrom, Write},
+    sync::Mutex,
+};
+
+use easy_fs::BlockDevice;
+
+const BLOCK_SZ: usize = 512;
+
+struct BlockFile(Mutex<File>);
+
+impl BlockDevice for BlockFile {
+    fn read_block(&self, block_id: usize, buf: &mut [u8]) {
+        let mut file = self.0.lock().unwrap();
+        file.seek(SeekFrom::Start((block_id * BLOCK_SZ) as u64))
+            .expect("Error when seeking!");
+        assert_eq!(file.read(buf).unwrap(), BLOCK_SZ, "Not a complete block!");
+    }
+
+    fn write_block(&self, block_id: usize, buf: &[u8]) {
+        let mut file = self.0.lock().unwrap();
+        file.seek(SeekFrom::Start((block_id * BLOCK_SZ) as u64))
+            .expect("Error when seeking!");
+        assert_eq!(file.write(buf).unwrap(), BLOCK_SZ, "Not a complete block!");
+    }
 }
+
+fn main() {}
