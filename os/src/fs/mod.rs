35966c1429bb47c946843f08d694505f626a8b60
vincenter <vincenterr@yahoo.com.tw>
2026-07-05 14:45:26 +0800

    feat: implement OSInode struct

--- os/src/fs/mod.rs
+++ os/src/fs/mod.rs
@@ -1,6 +1,10 @@
 use crate::mm::UserBuffer;
 
+mod inode;
+
 pub trait File: Send + Sync {
+    fn readable(&self) -> bool;
+    fn writable(&self) -> bool;
     fn read(&self, buf: UserBuffer) -> usize;
     fn write(&self, buf: UserBuffer) -> usize;
 }
